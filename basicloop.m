9da5b98b42619a475a5d5e165bcc0ab4b2d65d48
esn510sk <140237910+esn510sk@users.noreply.github.com>
2023-08-28 12:42:50 +0530

    Add files via upload

---
+++ basicloop.m
@@ -0,0 +1,31 @@
+i=0
+y=1
+for n=1:3
+    x=2*n^2+ n+1
+    disp(x)
+    
+
+    i=i+x
+    disp(i)
+    y=y*x
+    disp(y)
+end 
+close all
+clear all
+for n=1:5
+    if n>3
+        disp(n)
+    end
+end
+close all
+clear all 
+for n=1:10
+    disp(n)
+end
+close all 
+clear all
+for n=2:2:20
+    disp(n)
+end
+
+     
